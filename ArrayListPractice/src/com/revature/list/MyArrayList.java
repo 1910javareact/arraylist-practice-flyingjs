804885120badea76c57faec1e9516833e9bf715c
Joshua Roy <joshua.roy@revature.net>
2019-11-06 15:13:01 -0500

    first working solution

--- ArrayListPractice/src/com/revature/list/MyArrayList.java
+++ ArrayListPractice/src/com/revature/list/MyArrayList.java
@@ -2,29 +2,66 @@ package com.revature.list;
 
 public class MyArrayList {
 
+	int[] MyArray = new int[10];
+	int capacity = MyArray.length;
+	int usage = 0;
+
 	public void add(int val) {
-		// TODO Auto-generated method stub
-		
+
+		MyArray[usage] = val;
+		usage++;
+		if (usage >= capacity) {
+			capacity = capacity * 2;
+			int[] TempArray = new int[capacity];
+			for (int i = 0; i < MyArray.length; i++) {
+				TempArray[i] = MyArray[i];
+			}
+			MyArray = TempArray;
+		}
+
 	}
 
 	public void set(int index, int val) {
-		// TODO Auto-generated method stub
-		
+		if(index < 0 || index > usage) {
+			return;
+		}
+		MyArray[index] = val;
+
 	}
 
 	public void remove(int index) {
-		// TODO Auto-generated method stub
-		
+		if(index < 0 || index > usage) {
+			return;
+		}
+		for(int i = index; i < usage; i++) {
+			MyArray[i] = MyArray[i+1];
+		}
+		usage--;
+		if(usage < capacity / 2) {
+			capacity = capacity / 2;
+			int[] TempArray = new int[capacity];
+			for (int i = 0; i < MyArray.length / 2; i++) {
+				TempArray[i] = MyArray[i];
+			}
+			MyArray = TempArray;
+		}
+
 	}
 
 	public int get(int index) {
-		// TODO Auto-generated method stub
-		return 0;
+		if(index < 0 || index > usage) {
+			System.out.println("ArrayIndexOutOfBoundsTest");
+			return index;
+		}
+		return MyArray[index];
 	}
 
 	@Override
 	public String toString() {
-		// TODO Auto-generated method stub
-		return null;
+		String string = "";
+		for(int i = 0; i < usage; i++) {
+			string += Integer.toString(MyArray[i]);
+		}
+		return string;
 	}
 }
